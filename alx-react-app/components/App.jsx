7bc55bd859664117ff9b86c7971f36632d1d9410
Lebo Molaiwa <molaiwasharon@gmail.com>
2024-08-10 20:48:47 +0200

    fixed error

--- alx-react-app/components/App.jsx
+++ alx-react-app/components/App.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import React from "react";
+import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <UserProfile 
-              name="Alice" 
-              age="25" 
-              bio="Loves hiking and photography" 
-            />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
