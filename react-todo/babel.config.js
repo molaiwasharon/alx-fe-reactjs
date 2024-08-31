75fe6cfabf612217b9a0a0246590d18f39716f1d
Lebo Molaiwa <molaiwasharon@gmail.com>
2024-08-31 15:27:34 +0200

    fixed to do list

--- react-todo/babel.config.js
+++ react-todo/babel.config.js
@@ -2,9 +2,6 @@ module.exports = {
     presets: [
       '@babel/preset-env',
       '@babel/preset-react'
-    ],
-    plugins: [
-      '@babel/plugin-syntax-jsx'
     ]
   };
   
--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import TodoList from './component/TodoList';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="App">
+      <h1>Todo Application</h1>
+      <TodoList />  
     </div>
-    <div>
-    <h1>Todo List</h1>
-    <TodoList />
-  </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
